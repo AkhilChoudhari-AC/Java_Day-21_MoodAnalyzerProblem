587cde71e4624a542d18f5f400d2b7f99e21da84
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-23 21:19:37 +0530

    ADDED: Method just return sad to pass the Test

--- src/Com/BridgeLabz/MoodAnalyzer/MoodAnalyserTest.java
+++ src/Com/BridgeLabz/MoodAnalyzer/MoodAnalyserTest.java
@@ -17,7 +17,6 @@ public class MoodAnalyserTest {
         String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", actualResult);
     }
-
     @Test
     /**
      * created method analyseAnyMood() for getting mood as Happy
@@ -28,4 +27,5 @@ public class MoodAnalyserTest {
         String actualResult = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", actualResult);
     }
+
 }
